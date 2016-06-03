91f7f06ba3fde1f0e3918c5824630e6e2b6fd44c
stangar <simon.tang.arias@gmail.com>
2016-06-03 18:14:25 -0400

    still unfinished

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,11 +1,17 @@
 #item class
 
 class Item
-  attr_reader :name, :price, :imported
+  attr_reader :name, :price, :imported, :free_of_tax
   def initialize(name, price, imported)
     @name = name
     @price = price
     @imported = imported
+    @free_of_tax = free_of_tax
+  end
+#need to divide the responsability
+
+  def little_helper
+
   end
 
 end
--- sales_taxes1.rb
+++ sales_taxes1.rb
@@ -7,27 +7,46 @@ class Receipt
 
   def initialize
     @price = 0
+    @tax = 0
+    #an array to keep the items
+    @items = []
   end
 
+#a ver como se usa esto
+
+  def add_item(the_item_to_add)
+    @items << the_item_to_add
+  end
+#calcula el tax
   def tax(item)
+    free = item.free_of_tax
+    if free == false
     @price = item.price
-    @price = @price + (@price * 0.10)
+    @tax = @price * 0.10
+    @price = @price + @tax
+    end
     import_tax
   end
+#calcula el impuesto de importacion
 
   def import_tax(item)
     imported = item.imported
     if imported == true
+      @tax = @tax + (@price * 0.05)
       @price = @price + (@price *0.05)
     end
-    
   end
+
+#suma al total de la factura y el total de impuesto
+
   def add_to_total
     @@counter_total = @@counter_total + @price
-
+    @@counter_tax = @@counter_tax + @tax
   end
 end
 
 
 
-book = Item.new('book', 10, true)
+book = Item.new('book', 10, true, false)
+
+receipt.add_item(book)
